7b982585418aad0bdc914d0ab553a1ee0c15da54
Богдан <ress@ress.home>
2017-02-26 21:32:13 +0700

    Fix bug #1: in function -runnumber-.

--- unit.cpp
+++ unit.cpp
@@ -42,9 +42,9 @@ int checksum(int n, int a[])
 int runnumber(int n, int a[])
 {
     int numb = 1;
-    for(int i=0;i<n;++i)
+    for(int i=0;i<n-1;++i)
     {
-        if (a[i+1]<a[i]) numb++;
+        if (a[i+1]<a[i]) (numb++, cout << "Error " << a[i] << " " << a[i+1] << endl);
     }
     return numb;
 }
@@ -55,4 +55,5 @@ void printmas(int n, int a[])
     {
         cout<<a[i]<<" ";
     }
+    cout << dEndl;
 }
